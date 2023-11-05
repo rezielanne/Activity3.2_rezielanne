e27d07c443ca9ffe79002904fa82dc664950c63c
rezielanne <rezielanne19@gmail.com>
2023-11-05 21:12:57 +0800

    feat update delete inactive

--- hero.sql
+++ hero.sql
@@ -159,3 +159,11 @@ VALUES
  ALTER TABLE item_table
  ADD item_price DECIMAL(10,2); 
  
+ --4 Update state and Delete
+ UPDATE heroes
+SET state = 'inactive'
+WHERE hero_id = 1;
+
+DELETE FROM items
+WHERE hero_id = 1;
+  
